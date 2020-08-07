fb540f52a65bb1d9fad62f475557ce9ec6e43561
thisisaaronland <devnull@localhost>
2020-08-07 14:28:07 -0700

    add TemplatedFaultHandler; wrapper method to retrieve fault/error

--- fault.go
+++ fault.go
@@ -2,24 +2,64 @@ package fault
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"html/template"
 	"io"
 	"log"
 	"net/http"
 )
 
-const error_key string = "github.com/sfomuseum/go-http-fault#error"
-const status_key string = "github.com/sfomuseum/go-http-fault#status"
+const ErrorKey string = "github.com/sfomuseum/go-http-fault#error"
+const StatusKey string = "github.com/sfomuseum/go-http-fault#status"
+
+type FaultHandlerVars struct {
+	Status int
+	Error  error
+}
 
 func AssignError(req *http.Request, err error, status int) *http.Request {
 
 	ctx := req.Context()
-	ctx = context.WithValue(ctx, error_key, err)
-	ctx = context.WithValue(ctx, status_key, status)
+	ctx = context.WithValue(ctx, ErrorKey, err)
+	ctx = context.WithValue(ctx, StatusKey, status)
 	return req.WithContext(ctx)
 }
 
+func RetrieveError(req *http.Request) (int, error) {
+
+	ctx := req.Context()
+	err_v := ctx.Value(ErrorKey)
+	status_v := ctx.Value(StatusKey)
+
+	var status int
+	var err error
+
+	if err_v == nil {
+		msg := "FaultHandler triggered without an error context."
+		err = errors.New(msg)
+	} else {
+		err = err_v.(error)
+	}
+
+	if status_v == nil {
+		status = http.StatusInternalServerError
+	} else {
+		status = status_v.(int)
+	}
+
+	return status, err
+}
+
 func FaultHandler(wr io.Writer) (http.Handler, error) {
+	return faultHandler(wr, nil)
+}
+
+func TemplatedFaultHandler(wr io.Writer, t *template.Template) (http.Handler, error) {
+	return faultHandler(wr, t)
+}
+
+func faultHandler(wr io.Writer, t *template.Template) (http.Handler, error) {
 
 	prefix := ""
 	flags := log.LstdFlags
@@ -28,29 +68,29 @@ func FaultHandler(wr io.Writer) (http.Handler, error) {
 
 	fn := func(rsp http.ResponseWriter, req *http.Request) {
 
-		ctx := req.Context()
-		err_v := ctx.Value(error_key)
-		status_v := ctx.Value(status_key)
-
-		var msg string
-		var status int
-
-		if err_v == nil {
-			msg = "FaultHandler triggered without an error context."
-		} else {
-			err := err_v.(error)
-			msg = err.Error()
-		}
-
-		if status_v == nil {
-			status = http.StatusInternalServerError
-		} else {
-			status = status_v.(int)
-		}
+		status, err := RetrieveError(req)
+		msg := err.Error()
 
 		addr := req.RemoteAddr
 
 		l.Printf("[FAULT] %s \"%s %s %s\" %s\n", addr, req.Method, req.RequestURI, req.Proto, msg)
+
+		if t != nil {
+
+			vars := FaultHandlerVars{
+				Status: status,
+				Error:  err,
+			}
+
+			err = t.Execute(rsp, vars)
+
+			if err == nil {
+				return
+			}
+
+			msg := fmt.Sprintf("Failed to render template for fault handler, %v", err)
+			l.Printf("[FAULT] %s \"%s %s %s\" %s\n", addr, req.Method, req.RequestURI, req.Proto, msg)
+		}
 
 		err_msg := fmt.Sprintf("There was a problem completing your request (%d)", status)
 
