6ee1c437c721448e142bc069f0220fb1c0094cef
Aaron Straup Cope <thisisaaronland@users.noreply.github.com>
2023-06-06 13:46:24 -0700

    Add the ability to specify additional custom template variables (#2)
    * snapshot: something sort of merge-ish
    
    * merge might still be yak-shaving...
    
    * snapshot: working through FaultHandlerWithOptions
    
    * snapshot: working through reflect nonsense; this may not work...
    
    * snapshot: working through tests; still can't sort out interface conversion stuff...
    
    * snapshot: make it work with TestFaultHandlerWithCustomVars
    
    * make TestFaultHandlerVarsFunc pass again
    
    * snapshot: tests pass, amazingly
    
    * update docs; remove cmd
    
    ---------
    
    Co-authored-by: thisisaaronland <thisisaaronland@localhost>

--- fault_test.go
+++ fault_test.go
@@ -2,34 +2,12 @@ package fault
 
 import (
 	"fmt"
+	"html/template"
+	"io"
+	"log"
 	"net/http"
 	"testing"
 )
-
-func okHandler() http.Handler {
-
-	fn := func(rsp http.ResponseWriter, req *http.Request) {
-		return
-	}
-
-	return http.HandlerFunc(fn)
-}
-
-func errorHandler() http.Handler {
-
-	fn := func(rsp http.ResponseWriter, req *http.Request) {
-
-		err := fmt.Errorf("SAD FACE")
-		code := 999
-
-		AssignError(req, err, code)
-		rsp.WriteHeader(http.StatusBadRequest)
-
-		return
-	}
-
-	return http.HandlerFunc(fn)
-}
 
 func TestAssignError(t *testing.T) {
 
@@ -55,6 +33,122 @@ func TestAssignError(t *testing.T) {
 	}
 }
 
-func TestFaultHandler(t *testing.T) {
-	t.Skip()
+func TestFaultHandlerVarsFunc(t *testing.T) {
+
+	type ValidCustomVars struct {
+		Custom string
+		FaultHandlerVars
+	}
+
+	type InvalidCustomVars struct {
+		Custom string
+	}
+
+	valid_func := func() interface{} {
+
+		vars := &ValidCustomVars{
+			Custom: "custom",
+		}
+
+		return vars
+	}
+
+	invalid_func := func() interface{} {
+
+		vars := &InvalidCustomVars{
+			Custom: "custom",
+		}
+
+		return vars
+	}
+
+	valid_vars := valid_func()
+
+	if !ImplementsFaultHandlerVars(valid_vars) {
+		t.Fatalf("%T does not implement fault handler vars", valid_vars)
+	}
+
+	invalid_vars := invalid_func()
+
+	if ImplementsFaultHandlerVars(invalid_vars) {
+		t.Fatalf("%T implements fault handler vars, which is not expected", invalid_vars)
+	}
+}
+
+func TestFaultHandlerWithCustomVars(t *testing.T) {
+
+	tpl := template.New("test")
+	tpl, err := tpl.Parse(`{{ .Custom }} {{ .Status }}`)
+
+	if err != nil {
+		t.Fatalf("Failed to parse template, %v", err)
+	}
+
+	type CustomVars struct {
+		Custom string
+		FaultHandlerVars
+	}
+
+	custom_func := func() interface{} {
+
+		vars := &CustomVars{
+			Custom: "This is custom text",
+		}
+
+		return vars
+	}
+
+	opts := &FaultHandlerOptions{
+		Logger:   log.Default(),
+		Template: tpl,
+		VarsFunc: custom_func,
+	}
+
+	fh := FaultHandlerWithOptions(opts)
+
+	fn := func(rsp http.ResponseWriter, req *http.Request) {
+
+		err := fmt.Errorf("This is a test")
+		AssignError(req, err, 999)
+		fh.ServeHTTP(rsp, req)
+		return
+	}
+
+	h := http.HandlerFunc(fn)
+
+	mux := http.NewServeMux()
+	mux.Handle("/", h)
+
+	s := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	defer s.Close()
+
+	go func() {
+		s.ListenAndServe()
+	}()
+
+	rsp, err := http.Get("http://localhost:8080")
+
+	if err != nil {
+		t.Fatalf("Failed to get from localhost, %v", err)
+	}
+
+	defer rsp.Body.Close()
+
+	body, err := io.ReadAll(rsp.Body)
+
+	if err != nil {
+		t.Fatalf("Failed to read response, %v", err)
+	}
+
+	str_body := string(body)
+	expected_body := "This is custom text 999"
+
+	if str_body != expected_body {
+		t.Fatalf("Unexpected output '%s' (got '%s')", str_body, expected_body)
+	}
+
 }
